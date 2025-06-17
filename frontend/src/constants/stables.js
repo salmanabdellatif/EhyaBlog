25087a2ecda9ee71ff39b06da02ea5edeaf2c381
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2025-06-18 01:23:32 +0300

    update

--- frontend/src/constants/stables.js
+++ frontend/src/constants/stables.js
@@ -3,6 +3,5 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 
 const stables = { UPLOAD_FOLDER_BASE_URL, API_BASE_URL };
-console.log("Stables constants loaded:", stables);
 
 export default stables;
